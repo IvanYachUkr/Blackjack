8df4e95edc3c6c3cfe116d64f5168fef4c401136
Ivan Iachnik <86662751+IvanYachUkr@users.noreply.github.com>
2023-11-09 15:52:20 +0200

    Create Player.java
    Class keeps track of the each player`s state in the game session. It includes a current hand and actions that are available for each player

---
+++ Player.java
@@ -0,0 +1,23 @@
+public class Player {
+    private final Hand hand;
+
+    public Player() {
+        hand = new Hand();
+    }
+
+    public void hit(Card card) {
+        hand.addCard(card);
+    }
+
+    public boolean isBusted() {
+        return hand.getHandValue() > 21;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void clearHand() {
+        hand.clear();
+    }
+}

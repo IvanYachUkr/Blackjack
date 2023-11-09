acf814691c5c43b269a72c5d49601b95f280f7a5
Ivan Iachnik <86662751+IvanYachUkr@users.noreply.github.com>
2023-11-09 16:01:47 +0200

    Create RequestHandler.java
    Class processes initial request form the client, checks the id, separates the command to execute

---
+++ RequestHandler.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+
+public class RequestHandler {
+    private Map<String, GameSession> sessionMap;
+
+    public RequestHandler(Map<String, GameSession> sessionMap) {
+        this.sessionMap = sessionMap;
+    }
+
+    public String handleRequest(String request) {
+        // The request pattern is "playerId:command"
+        String[] parts = request.split(":");
+        if (parts.length != 2) {
+            return "Error: Invalid request format.";
+        }
+
+        String playerId = parts[0];
+        String command = parts[1];
+
+        GameSession session = sessionMap.computeIfAbsent(playerId, k -> new GameSession());
+        CommandProcessor processor = new CommandProcessor(session);
+        return processor.processCommand(playerId, command);
+    }
+}

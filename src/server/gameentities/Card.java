ad2a26e43a4058068be4b6b7ed52624bd5df31ab
Ors1mer <ors1mer_dev@proton.me>
2023-11-16 00:31:28 +0200

    Change card printing + update Makefile

--- src/server/gameentities/Card.java
+++ src/server/gameentities/Card.java
@@ -10,6 +10,11 @@ public class Card {
         JACK, QUEEN, KING, ACE
     }
 
+    private static final char heart = '\u2665';
+    private static final char diamond = '\u2666';
+    private static final char club = '\u2663';
+    private static final char spade = '\u2660';
+
     private final Suit suit;
     private final Rank rank;
 
@@ -38,6 +43,30 @@ public class Card {
 
     @Override
     public String toString() {
-        return rank + " of " + suit;
+        char suit_sign = heart;
+        switch (suit) {
+            case DIAMONDS:
+                suit_sign = diamond; break;
+            case CLUBS:
+                suit_sign = club; break;
+            case SPADES:
+                suit_sign = spade; break;
+        }
+        switch (rank) {
+            case TEN:
+                return "10" + suit_sign;
+            case JACK:
+                return "J" + suit_sign;
+            case QUEEN:
+                return "Q" + suit_sign;
+            case KING:
+                return "K" + suit_sign;
+            case ACE:
+                return "A" + suit_sign;
+            default: // case of 2-9
+                return String.valueOf((char)( '2'+rank.ordinal() )) +
+                       String.valueOf(suit_sign);
+        }
     }
 }
+
